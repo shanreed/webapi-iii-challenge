906b1302907c3228a8472882916f14093edf4ce5
shanreed <srtech5set@gmail.com>
2019-06-05 14:20:23 -0700

    added morgan and helmet middlewares

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const helmet = require('helmet');
+const logger = require('morgan');
+
 
 const postsRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter')
 
 const server = express();
 
+//Middleware
 server.use(express.json());
 server.use(helmet());
+server.use(logger('dev'));
+server.use(customLogger);
+
 
 server.use('/api/post', postsRouter);
-server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -17,7 +23,7 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {
+function customLogger(req, res, next) {
  console.log(`${req.method}, ${req.url}, ${new Date().toISOString()} `)
  next();
 };
