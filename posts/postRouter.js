293d9f825e47324e8a3b995cc6d623f35efaf2e5
shanreed <srtech5set@gmail.com>
2019-06-07 07:38:52 -0700

    added validatePostId middleware

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -37,7 +37,18 @@ router.put('/:id', (req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
-
-};
-
+  const  id  = req.params;
+  PostDb.getById(id)
+    .then(post => {
+      if (post) {
+        req.post = post;
+      } else {
+        res.status(404).json({ message: "No post with that id was found" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Error getting post." });
+    });
+}
 module.exports = router;
